ad8dd99f0557958b6b45c42ab8a8194ee8762d4e
gileduardo <gilgea@gmail.com>
2026-05-31 17:44:21 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
         }),
-        tailwindcss(),
     ],
     server: {
+        hmr: {
+            host: 'localhost',
+        },
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            usePolling: true, // Garante que o Vite perceba mudanças de arquivos no Docker (Windows/WSL)
         },
     },
 });
